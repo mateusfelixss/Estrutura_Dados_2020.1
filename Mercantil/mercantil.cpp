bb02a927e5d72dc44cf9fc65fe12551a2d69b9c8
mfelixss <mateusffelix.ss@gmail.com>
2020-12-03 19:05:13 -0300

    mercantil inicio

---
+++ Mercantil/mercantil.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    //cout << "Qtd de apostas: ";
+    //cin >> n;
+    double valores[n];      //valores das mercadorias
+    double chutes[n];       //chutes de valores do player 1
+    char apostas[n]         //palpite de Mais ou Menos do player 2
+
+    /*
+    for (int i = 0; i < n; i++){
+        cin >> valores[i];
+    }
+
+    cout << "Maior ou meno? ";
+    for (int i = 0; i < n; i++){
+        cin >> apostas[i];
+    }
+    */
+}
